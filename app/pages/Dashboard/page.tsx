030b1d8e6ba3133b6204f8356e970c4b140870d5
tefots <tefotsiu0@gmail.com>
2024-11-30 10:58:05 +0200

    icons sizes

--- app/pages/Dashboard/page.tsx
+++ app/pages/Dashboard/page.tsx
@@ -2,16 +2,16 @@ import { DashboardLayout } from './../../components/DashboardComponents/Dashboar
 import { StatsCard } from './../../components/DashboardComponents/StatsCard'
 import { RecentActivity } from './../../components/DashboardComponents/RecentActivity'
 import { DataTable } from './../../components/DashboardComponents/DataTable'
-import { Users, ShoppingCart, DollarSign, TrendingUp, Book } from 'lucide-react'
+import { Users, ShoppingCart, DollarSign, TrendingUp, Book, FileText, Package, CreditCard } from 'lucide-react'
 
 export default function DashboardPage() {
   return (
     <DashboardLayout>
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-        <StatsCard title="Price Book"   icon={<Book size={24} />} />
-        <StatsCard title="Reports"  icon={<ShoppingCart size={24} />} />
-        <StatsCard title=""  icon={<DollarSign size={24} />} />
-        <StatsCard title="Growth"  icon={<TrendingUp size={24} />} />
+        <StatsCard title="Price Book"   icon={<Book size={30} />} />
+        <StatsCard title="Reports"  icon={<FileText size={30} />} />
+        <StatsCard title="Stock Levels"  icon={<Package size={30} />} />
+        <StatsCard title="POS"  icon={<CreditCard size={30} />} />
       </div>
       <div className="grid gap-4 mt-4 md:grid-cols-2">
         <RecentActivity />
