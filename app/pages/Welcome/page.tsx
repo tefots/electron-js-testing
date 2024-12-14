fa9b248da579e5e5bc7785ffbfaa29c3c83b49b5
tefots <tefotsiu0@gmail.com>
2024-12-14 16:23:17 +0200

    color change on the welcome

--- app/pages/Welcome/page.tsx
+++ app/pages/Welcome/page.tsx
@@ -4,12 +4,17 @@ import Link from "next/link";
 export default function Welcome(){
 
     return(
-        <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-        <div className="container mx-auto p-6 text-center">
-          <h1 className="text-5xl font-bold text-blue-600 mb-6">
+        <div className=" relative bg-[#380a8e] min-h-screen flex items-center justify-center">
+
+      {/* Overlay to darken the video */}
+{/* <div className="absolute inset-0 bg-black opacity-25 z-0"></div> */}
+
+
+        <div className=" container mx-auto p-6 text-center">
+          <h1 className="text-5xl font-bold text-blue-200 mb-6">
             Welcome to Tequila Tech Solutions POS System
           </h1>
-          <p className="text-lg text-gray-600 mb-6">
+          <p className="text-lg text-gray-100 mb-6">
             Streamline your sales process with our easy-to-use Point of Sale solution.
           </p>
           <Link
