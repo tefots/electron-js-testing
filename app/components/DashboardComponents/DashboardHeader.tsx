45dba1b61cf0c0931c6670d99df906317d7f3104
tefots <tefotsiu0@gmail.com>
2024-12-14 18:44:54 +0200

    adding the search

--- app/components/DashboardComponents/DashboardHeader.tsx
+++ app/components/DashboardComponents/DashboardHeader.tsx
@@ -1,11 +1,50 @@
+"use client";
+
 import { Bell, User } from 'lucide-react'
 import { Button } from '@/components/ui/button'
+import React, { useState, useEffect} from 'react'
 
 export function DashboardHeader() {
+  const [categories, setCategories]= useState([
+    'Fruit Trees', 'Forest Trees', 'Flowers'
+  ]);
+  const [selectedCategory, setSelectedCategory] = useState ('');
+  const [results, setResults] = useState([]);
+
+  const handleCategoryChange = (category: React.SetStateAction<string>) => {
+    setSelectedCategory(category);
+
+    //code to fetch product based on the category selected
+    
+
+  }
   return (
     <header className="bg-white shadow-sm p-4">
       <div className="flex justify-between items-center">
-        <h1 className="text-2xl font-semibold">Dashboard</h1>
+        <h1 className="text-2xl font-semibold">Lesotho Nursery Shop</h1>
+        <div className="flex flex-col md:flex-row bg-slate-50  mx-5 rounded-xl">
+            <div className="p-1 ">
+              <input 
+               type='text'
+               placeholder='Search by category / name'
+               className="border-slate-200 border rounded-lg text-start p-2"
+               />
+            </div>
+
+            <div className='px-2'>
+              <select 
+              id="category"
+              value={selectedCategory}
+              onChange={(e) => handleCategoryChange(e.target.value)}
+              className="w-full h-10 mt-1 ps-2 pr-2 rounded-lg bg-slate-100">
+                <option value={""}>Select Category</option>
+                {categories.map((cat, index) => (
+                  <option key={index} value={cat}>{cat}</option>
+                ))}
+              
+              </select>
+            </div>
+        </div>
         <div className="flex items-center space-x-4">
           <Button variant="ghost" size="icon">
             <Bell size={20} />
