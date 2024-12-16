f684ae4c4b058fa397a75b5ef3f2cd2db36c20c4
tefots <tefotsiu0@gmail.com>
2024-12-16 22:52:52 +0200

    background color update

--- app/components/SidebarComponents/Products.tsx
+++ app/components/SidebarComponents/Products.tsx
@@ -9,9 +9,9 @@ export default function ProductsPage({title, icon}: productProps ) {
       <div className="p-2">
         {/* top of the products that provides the description*/}
 
-        <Card className="flex flex-row  bg-blue-50 ">
+        <Card className="flex flex-row  bg-white p-2">
           <CardHeader className="ms-2">{icon}</CardHeader>
-          <CardTitle className="text-lg items-center space-y-2 mt-4 p-1 ms-0 mr-2 font-medium text-green-600">{title}</CardTitle>
+          <CardTitle className="text-lg items-center space-y-2 mt-4 p-1 ms-0 mr-2 font-medium text-black-400">{title}</CardTitle>
 
         </Card>
       </div>
