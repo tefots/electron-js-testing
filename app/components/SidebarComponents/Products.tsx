2c61b62b56ce32948db8910491c873ac8b199fdc
tefots <tefotsiu0@gmail.com>
2024-12-16 22:40:39 +0200

    products page update

--- app/components/SidebarComponents/Products.tsx
+++ app/components/SidebarComponents/Products.tsx
@@ -1,8 +1,19 @@
-export default function ProductsPage() {
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
+interface productProps {
+  title: String,
+  icon: React.ReactNode
+}
+export default function ProductsPage({title, icon}: productProps ) {
     return (
-      <div>
-        <h1 className="text-2xl font-bold mb-4">Products</h1>
-        <p>Manage your product inventory here.</p>
+      <div className="p-2">
+        {/* top of the products that provides the description*/}
+
+        <Card className="flex flex-row  bg-blue-50 ">
+          <CardHeader className="ms-2">{icon}</CardHeader>
+          <CardTitle className="text-lg items-center space-y-2 mt-4 p-1 ms-0 mr-2 font-medium text-green-600">{title}</CardTitle>
+
+        </Card>
       </div>
     );
   }
--- app/pages/Products/page.tsx
+++ app/pages/Products/page.tsx
@@ -1,4 +1,6 @@
 import { DashboardLayout } from "@/app/components/DashboardComponents/DashboardLayout"
+import ProductsPage from "@/app/components/SidebarComponents/Products"
+import { Settings } from "lucide-react"
 
 
 export default function Products (){
@@ -7,8 +9,13 @@ export default function Products (){
     return(
         <>
         <DashboardLayout >
-        <div>
-            <p>Your products here</p>
+        <h1 className="text-2xl font-bold mb-4">Products</h1>
+        <p>Manage your product inventory here.</p>
+        <div className="flex flex-col md:flex-row">
+            <ProductsPage icon={<Settings size= {20} /> } title ="Fruit Trees" />
+            <ProductsPage icon={<Settings size= {20} /> } title ="Stetings" />
+            <ProductsPage icon={<Settings size= {20} /> } title ="Stetings" />
+            <ProductsPage icon={<Settings size= {20} /> } title ="Stetings" />
         </div>
         </DashboardLayout>
         </>
