6ee5cef0a8eeb7e699c7ffc6d9a4093cea124c8f
tefots <tefotsiu0@gmail.com>
2024-12-23 21:21:58 +0200

    add product button

--- app/pages/Products/page.tsx
+++ app/pages/Products/page.tsx
@@ -1,24 +1,43 @@
+"use client";
 import { DashboardLayout } from "@/app/components/DashboardComponents/DashboardLayout"
 import ProductsPage from "@/app/components/SidebarComponents/Products"
 import ProductsCards from "@/app/components/SidebarComponents/productsCards"
-import { Book, PencilIcon, Settings } from "lucide-react"
+import { Book, Flower, Flower2, Heart, PencilIcon, Settings, Trees } from "lucide-react"
 import Image from "next/image"
+import { useRouter } from "next/navigation"
+
 import { title } from "process"
 
 
 export default function Products (){
 
-
+    // defining method to add product into the database
+    function addProduct(){
+        const route = useRouter();
+        route.push('/pages/Products/Add');
+    }
     return(
         <>
         <DashboardLayout >
-        <h1 className="text-2xl font-bold mb-4">Products</h1>
-        <p>Manage your product inventory here.</p>
+            <div className="flex ">
+                
+                    <h1 className="text-2xl font-bold mb-4">Products</h1>
+                    <p>Manage your product inventory here.</p>
+            
+            </div>
+
         <div className="flex flex-col md:flex-row">
             <ProductsPage icon={<Book size= {20} /> } title ="Fruit Trees" />
-            <ProductsPage icon={<Settings size= {20} /> } title ="Forest" />
-            <ProductsPage icon={<Settings size= {20} /> } title ="Flowers" />
-            <ProductsPage icon={<Settings size= {20} /> } title ="Decorations" />
+            <ProductsPage icon={<Trees size= {20} /> } title ="Forest" />
+            <ProductsPage icon={<Flower size= {20} /> } title ="Flowers" />
+            <ProductsPage icon={<Flower2 size= {20} /> } title ="Herbs" />
+
+            <div>
+
+            <button className="mx-2 bg-slate-500  mt-6  p-3 rounded-2xl text-white" onClick={() => addProduct()}>
+                        + Add Product
+            </button>
+            </div>
         </div>
 
         {/* products cards with images and numbers */}
