b3e111958cb0fa38a91941a6f29882b44ae04a46
tefots <tefotsiu0@gmail.com>
2025-01-18 11:43:12 +0200

    sqlite installation

--- app/pages/Pos/page.tsx
+++ app/pages/Pos/page.tsx
@@ -153,7 +153,6 @@ const POSPage = () => {
       </div>
     </div>
     </DashboardLayout>
-
   );
 };
 
---
+++ app/utils/dbConnection.ts
@@ -0,0 +1,16 @@
+import Database from 'better-sqlite3';
+
+const db = new Database('./database.db', { verbose: console.log });
+
+// Create the "products" table if it doesn't exist
+db.prepare(`
+  CREATE TABLE IF NOT EXISTS products (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    productName TEXT NOT NULL,
+    quantity INTEGER NOT NULL,
+    price REAL NOT NULL,
+    imagePath TEXT
+  )
+`).run();
+
+export default db;
