4aa4b8484940d810709c45dee1f8a64ba2800d62
tefots <tefotsiu0@gmail.com>
2025-04-29 10:50:15 +0200

    debugging

--- app/components/SidebarComponents/Interfaces/POSsidebar.tsx
+++ app/components/SidebarComponents/Interfaces/POSsidebar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 import { useState } from "react";
 
 interface POSProps {
