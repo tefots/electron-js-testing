83887f3fb258d0e1657cdb07526389ceb9b400f6
tefots <tefotsiu0@gmail.com>
2025-04-29 10:47:34 +0200

    debugging

--- app/components/DashboardComponents/StatsCard.tsx
+++ app/components/DashboardComponents/StatsCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardHeader, CardTitle } from '@/components/ui/card'
 import Link from 'next/link'
 
 interface StatsCardProps {
