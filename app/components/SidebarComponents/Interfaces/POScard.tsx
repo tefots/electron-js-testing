9852bd1dbc03cdf7f431e4dddc0c0bd96f4390f8
tefots <tefotsiu0@gmail.com>
2025-04-29 10:49:25 +0200

    debugging

--- app/components/SidebarComponents/Interfaces/POScard.tsx
+++ app/components/SidebarComponents/Interfaces/POScard.tsx
@@ -1,14 +1,11 @@
 import { Card, CardContent, CardHeader } from "@/components/ui/card"
-import { title } from "process"
-import { ReactNode } from "react"
+
 
 interface posProps{
     image: React.ReactNode
     icon: React.ReactNode
     title: String
     price: number
-
-
 }
 
 export default function POSCard({image, icon, title, price}: posProps){
